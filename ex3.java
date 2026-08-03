a158918f513afa1d701a6dacc604c43e9200e4ce
João Harth <joaotessis@gmail.com>
2026-08-03 20:21:03 -0300

    Add files via upload

--- ex3.java
+++ ex3.java
@@ -10,26 +10,26 @@ public class ex3 {
 		
 		System.out.println("DIgite um numero");
 		
-		int n1 = leitor.nextInt();
+		float n1 = leitor.nextInt();
 		
 		System.out.println("Digite outro número");
 		
-		int n2 = leitor.nextInt();
+		float n2 = leitor.nextInt();
 		
 		
-		int calculo = n1 / n2; 
+		float calculo = n1 / n2; 
 				
 		if (calculo == 1) {
 		System.out.println("Os números são iguais");
 		
 		} else  {
 			System.out.println("Os números são diferentes");
-		} if (n1 > n2) { 
+		 if (n1 > n2) { 
 			System.out.println("O maior número é:" + n1);
-			
+		 	
 		} else {
 			System.out.println("O maior número é:"+ n2);
-			
+		}	
 		}
 		// TODO Auto-generated method stub
 
